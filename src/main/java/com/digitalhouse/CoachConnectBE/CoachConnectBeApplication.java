fab8c446ace4f793a7c94857391e60a444aa6175
darwi <darwinsosa2004@gmail.com>
2024-02-18 17:18:12 -0300

    16: added base files to the project

---
+++ src/main/java/com/digitalhouse/CoachConnectBE/CoachConnectBeApplication.java
@@ -0,0 +1,13 @@
+package com.digitalhouse.CoachConnectBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoachConnectBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoachConnectBeApplication.class, args);
+	}
+
+}
