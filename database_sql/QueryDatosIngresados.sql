32f6788cf7eec050d9f015de717995363c01085e
MCristina94 <cmaureen1908@gmail.com>
2024-02-22 11:28:18 -0500

    Se realizaron cambios sugeridos por el area de backend: camelcase, nombre del proyecto y eliminacion del rol

--- database_sql/QueryDatosIngresados.sql
+++ database_sql/QueryDatosIngresados.sql
@@ -1,37 +1,35 @@
 /*1. Se crean usuarios para pruebas*/
-insert into usuario (Nombre, Apellido, Edad, Email, ContactoCelular, Foto, Username, Password, Rol) values ('Maria Jose', 'Morales', 19, 'mariajose@gmail.com', '+573104587452', 'https://images.unsplash.com/photo-1554151228-14d9def656e4?q=80&w=1372&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', 'majo', '1234', 'Estudiante');
-insert into usuario (Nombre, Apellido, Edad, Email, ContactoCelular, Foto, Username, Password, Rol) values ('Ramona', 'Gonzalez', 29, 'ramona@gmail.com', '+573112547852', 'https://plus.unsplash.com/premium_photo-1661772932347-8f23d66165ad?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', 'ramona', '1234', 'Estudiante');
-insert into usuario (Nombre, Apellido, Edad, Email, ContactoCelular, Foto, Username, Password, Rol) values ('Lia', 'Ruales', 22, 'liarua@gmail.com', '+573236587412', 'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', 'liarua', '1234', 'Estudiante');
-insert into usuario (Nombre, Apellido, Edad, Email, ContactoCelular, Foto, Username, Password, Rol) values ('Antonio', 'Rodriguez', 39, 'antonio@gmail.com', '+573167548969', 'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', 'antonio', '1234', 'Tutor');
-insert into usuario (Nombre, Apellido, Edad, Email, ContactoCelular, Foto, Username, Password, Rol) values ('Diego Armando', 'Silva', 22, 'diegosilva@gmail.com', '+573112547852', 'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', 'diego', '1234', 'tutor');
-insert into usuario (Nombre, Apellido, Edad, Email, ContactoCelular, Foto, Username, Password, Rol) values ('Juan', 'Pérez', 35, 'juanperez@example.com', '+573001234567', 'https://images.unsplash.com/photo-1513956589380-bad6acb9b9d4?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', 'juanito', 'password123', 'Estudiante');
-insert into usuario (Nombre, Apellido, Edad, Email, ContactoCelular, Foto, Username, Password, Rol) values ('Carlos', 'González', 42, 'cgonzalez@example.com', '+573009876543', 'https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', 'cgonz', 'contraseña123', 'Administrador');
-insert into usuario (Nombre, Apellido, Edad, Email, ContactoCelular, Foto, Username, Password, Rol) values ('Ana', 'López', 28, 'ana.lopez@example.com', '+573005678901', 'https://plus.unsplash.com/premium_photo-1691622500807-6d9eeb9ea06a?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', 'anita', 'claveSegura', 'Tutor');
-insert into usuario (Nombre, Apellido, Edad, Email, ContactoCelular, Foto, Username, Password, Rol) values  ('Laura', 'Martínez', 30, 'lauramartinez@example.com', '+573001112233', 'https://images.unsplash.com/photo-1548733865-2ff7802d2b3f?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', 'lauram', 'claveLaura123', 'Estudiante');
-insert into usuario (Nombre, Apellido, Edad, Email, ContactoCelular, Foto, Username, Password, Rol) values  ('Alejandro', 'Rodríguez', 25, 'alejandrorodriguez@example.com', '+573004445566', 'https://images.unsplash.com/photo-1608681299041-cc19878f79f1?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', 'alejandro.r', 'passwordAle123', 'Tutor');
+insert into usuario (Nombre, Apellido, Edad, Email, ContactoCelular, Foto, UserName, Password) values ('Maria Jose', 'Morales', 19, 'mariajose@gmail.com', '+573104587452', 'https://images.unsplash.com/photo-1554151228-14d9def656e4?q=80&w=1372&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', 'majo', '1234');
+insert into usuario (Nombre, Apellido, Edad, Email, ContactoCelular, Foto, Username, Password) values ('Ramona', 'Gonzalez', 29, 'ramona@gmail.com', '+573112547852', 'https://plus.unsplash.com/premium_photo-1661772932347-8f23d66165ad?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', 'ramona', '1234');
+insert into usuario (Nombre, Apellido, Edad, Email, ContactoCelular, Foto, Username, Password) values ('Lia', 'Ruales', 22, 'liarua@gmail.com', '+573236587412', 'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', 'liarua', '1234');
+insert into usuario (Nombre, Apellido, Edad, Email, ContactoCelular, Foto, Username, Password) values ('Antonio', 'Rodriguez', 39, 'antonio@gmail.com', '+573167548969', 'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', 'antonio', '1234');
+insert into usuario (Nombre, Apellido, Edad, Email, ContactoCelular, Foto, Username, Password) values ('Diego Armando', 'Silva', 22, 'diegosilva@gmail.com', '+573112547852', 'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', 'diego', '1234');
+insert into usuario (Nombre, Apellido, Edad, Email, ContactoCelular, Foto, Username, Password) values ('Juan', 'Pérez', 35, 'juanperez@example.com', '+573001234567', 'https://images.unsplash.com/photo-1513956589380-bad6acb9b9d4?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', 'juanito', 'password123');
+insert into usuario (Nombre, Apellido, Edad, Email, ContactoCelular, Foto, Username, Password) values ('Carlos', 'González', 42, 'cgonzalez@example.com', '+573009876543', 'https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', 'cgonz', 'contraseña123');
+insert into usuario (Nombre, Apellido, Edad, Email, ContactoCelular, Foto, Username, Password) values ('Ana', 'López', 28, 'ana.lopez@example.com', '+573005678901', 'https://plus.unsplash.com/premium_photo-1691622500807-6d9eeb9ea06a?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', 'anita', 'claveSegura');
+insert into usuario (Nombre, Apellido, Edad, Email, ContactoCelular, Foto, Username, Password) values  ('Laura', 'Martínez', 30, 'lauramartinez@example.com', '+573001112233', 'https://images.unsplash.com/photo-1548733865-2ff7802d2b3f?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', 'lauram', 'claveLaura123');
+insert into usuario (Nombre, Apellido, Edad, Email, ContactoCelular, Foto, Username, Password) values  ('Alejandro', 'Rodríguez', 25, 'alejandrorodriguez@example.com', '+573004445566', 'https://images.unsplash.com/photo-1608681299041-cc19878f79f1?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', 'alejandro.r', 'passwordAle123');
 
 /*2. Se crean estudiantes para pruebas*/
-insert into estudiante (nivel_Educativo, Usuario_idUsuario) values ('Bachiller', 1);
-insert into estudiante (nivel_Educativo, Usuario_idUsuario) values ('Universitario', 2);
-insert into estudiante (nivel_Educativo, Usuario_idUsuario) values ('Bachiller', 3);
-insert into estudiante (nivel_Educativo, Usuario_idUsuario) values ('Primaria', 4);
-insert into estudiante (nivel_Educativo, Usuario_idUsuario) values ('Universitario', 5);
-insert into estudiante (nivel_Educativo, Usuario_idUsuario) values ('Bachiller', 8);
-insert into estudiante (nivel_Educativo, Usuario_idUsuario) values ('Bachiller', 11);
+insert into estudiante (nivelEducativo, Usuario_idUsuario) values ('Bachiller', 1);
+insert into estudiante (nivelEducativo, Usuario_idUsuario) values ('Universitario', 2);
+insert into estudiante (nivelEducativo, Usuario_idUsuario) values ('Bachiller', 3);
+insert into estudiante (nivelEducativo, Usuario_idUsuario) values ('Primaria', 4);
+insert into estudiante (nivelEducativo, Usuario_idUsuario) values ('Universitario', 5);
+insert into estudiante (nivelEducativo, Usuario_idUsuario) values ('Bachiller', 8);
 
 /*3. Se crean tutores para pruebas*/
 insert into tutor (Profesion, Descripcion, Usuario_idUsuario) values ('Licenciado en matematicas','Poseo un profundo entendimiento de conceptos matemáticos fundamentales, incluyendo álgebra, cálculo, geometría, análisis numérico, teoría de números, probabilidad y estadística.', 6);
 insert into tutor (Profesion, Descripcion, Usuario_idUsuario) values ('Licenciado en lenguas','cuento con un dominio sólido de uno o más idiomas, incluyendo gramática, vocabulario, pronunciación y habilidades de comprensión auditiva y expresión escrita y oral.', 7);
 insert into tutor (Profesion, Descripcion, Usuario_idUsuario) values ('Ingeniero Informatico', 'Competencia en el diseño, desarrollo, implementación y mantenimiento de aplicaciones y sistemas de software, utilizando diversos lenguajes de programación, frameworks y tecnologías.', 10);
-insert into tutor (Profesion, Descripcion, Usuario_idUsuario) values ('Licenciado en literatura','Poseo un conocimiento exhaustivo de la literatura mundial, incluyendo obras clásicas y contemporáneas, así como diferentes géneros literarios como la novela, poesía, ensayo, teatro, entre otros.' , 12);
 
 /*4. Se crean administradores para pruebas*/
 insert into admin (Usuario_idUsuario) values (9);
 
 /*5. Se crea detalle de reserva para pruebas*/
-insert into detalle_reserva (Fecha_Inicio_Reserva, Fecha_Fin_Reserva, Horas_Reservadas, Estudiante_idEstudiante, Tutor_idTutor) values ('2024-02-19 9:00:00', '2024-02-28 9:00:00',216.00,1, 2);
-insert into detalle_reserva (Fecha_Inicio_Reserva, Fecha_Fin_Reserva, Horas_Reservadas, Estudiante_idEstudiante, Tutor_idTutor) values ('2024-03-19 9:00:00', '2024-03-30 9:00:00',264.00, 3, 4);
-insert into detalle_reserva (Fecha_Inicio_Reserva, Fecha_Fin_Reserva, Horas_Reservadas, Estudiante_idEstudiante, Tutor_idTutor) values ('2024-04-19 9:00:00', '2024-04-30 9:00:00',264.00,5, 1);
+insert into detalle_reserva (FechaInicioReserva, FechaFinReserva, HorasReservadas, Estudiante_idEstudiante, Tutor_idTutor) values ('2024-02-19 9:00:00', '2024-02-28 9:00:00',216.00,1, 2);
+insert into detalle_reserva (FechaInicioReserva, FechaFinReserva, HorasReservadas, Estudiante_idEstudiante, Tutor_idTutor) values ('2024-03-19 9:00:00', '2024-03-30 9:00:00',264.00, 3, 3);
+insert into detalle_reserva (FechaInicioReserva, FechaFinReserva, HorasReservadas, Estudiante_idEstudiante, Tutor_idTutor) values ('2024-04-19 9:00:00', '2024-04-30 9:00:00',264.00,5, 1);
 
 /*6. Se crea nivel  para pruebas*/
 insert into nivel (Nombre) values ('Primaria');
@@ -40,7 +38,6 @@ insert into nivel (Nombre) values ('Universidad');
 insert into nivel (Nombre) values ('Hobby');
 
 /*7. Se crea tutorias para pruebas*/
-
 insert into tutoria (Nombre, Descripcion, Nivel_idNivel) values ('Matematicas', 'se enfocan en conceptos como álgebra, geometría, trigonometría, cálculo y estadística, que proporcionan herramientas para resolver problemas y comprender el mundo que nos rodea de manera cuantitativa.', 2);
 insert into tutoria (Nombre, Descripcion, Nivel_idNivel) values ('Biologia', 'La biología explora la vida: plantas, animales, células y ecosistemas, promoviendo la comprensión del mundo natural.', 1);
 insert into tutoria (Nombre, Descripcion, Nivel_idNivel) values ('Quimica', 'Profundiza en la estructura, propiedades y transformaciones de la materia, desde átomos hasta materiales complejos, abordando reacciones, energía y aplicaciones en diversas áreas.', 3);
@@ -48,7 +45,8 @@ insert into tutoria (Nombre, Descripcion, Nivel_idNivel) values ('Castellano', '
 insert into tutoria (Nombre, Descripcion, Nivel_idNivel) values ('Arte Plastica', 'Las artes plásticas como hobby abarcan formas de expresión visual como pintura, dibujo, escultura y fotografía, permitiendo explorar la creatividad y desarrollar habilidades.', 4);
 
 /*8. Se crea tutor_tutorias para pruebas*/
-insert into tutor_tutoria (Tutor_idTutor, Tutoria_idTutoria) values (6, 1);
-insert into tutor_tutoria (Tutor_idTutor, Tutoria_idTutoria) values (7, 4);
-insert into tutor_tutoria (Tutor_idTutor, Tutoria_idTutoria) values (4, 4);
+insert into tutor_tutoria (Tutor_idTutor, Tutoria_idTutoria) values (1, 1);
+insert into tutor_tutoria (Tutor_idTutor, Tutoria_idTutoria) values (2, 4);
+
+
 
--- database_sql/ScriptCreacionDeTablas.sql
+++ database_sql/ScriptCreacionDeTablas.sql
@@ -5,20 +5,20 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema BookingTutorias
+-- Schema CoachConnect
 -- -----------------------------------------------------
-DROP SCHEMA IF EXISTS `BookingTutorias` ;
+DROP SCHEMA IF EXISTS `CoachConnect` ;
 
 -- -----------------------------------------------------
--- Schema BookingTutorias
+-- Schema CoachConnect
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `BookingTutorias` DEFAULT CHARACTER SET utf8 ;
-USE `BookingTutorias` ;
+CREATE SCHEMA IF NOT EXISTS `CoachConnect` DEFAULT CHARACTER SET utf8 ;
+USE `CoachConnect` ;
 
 -- -----------------------------------------------------
--- Table `BookingTutorias`.`Usuario`
+-- Table `CoachConnect`.`Usuario`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `BookingTutorias`.`Usuario` (
+CREATE TABLE IF NOT EXISTS `CoachConnect`.`Usuario` (
   `idUsuario` INT NOT NULL AUTO_INCREMENT,
   `Nombre` VARCHAR(45) NOT NULL,
   `Apellido` VARCHAR(45) NOT NULL,
@@ -26,35 +26,34 @@ CREATE TABLE IF NOT EXISTS `BookingTutorias`.`Usuario` (
   `Email` VARCHAR(45) NOT NULL,
   `ContactoCelular` VARCHAR(45) NOT NULL,
   `Foto` VARCHAR(200) NOT NULL,
-  `Username` VARCHAR(45) NOT NULL,
+  `UserName` VARCHAR(45) NOT NULL,
   `Password` VARCHAR(45) NOT NULL,
-  `Rol` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`idUsuario`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `BookingTutorias`.`Estudiante`
+-- Table `CoachConnect`.`Estudiante`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `BookingTutorias`.`Estudiante` (
+CREATE TABLE IF NOT EXISTS `CoachConnect`.`Estudiante` (
   `idEstudiante` INT NOT NULL AUTO_INCREMENT,
-  `Nivel_Educativo` VARCHAR(45) NOT NULL,
+  `NivelEducativo` VARCHAR(45) NOT NULL,
   `Usuario_idUsuario` INT NOT NULL,
   PRIMARY KEY (`idEstudiante`, `Usuario_idUsuario`),
   CONSTRAINT `fk_Estudiante_Usuario1`
     FOREIGN KEY (`Usuario_idUsuario`)
-    REFERENCES `BookingTutorias`.`Usuario` (`idUsuario`)
+    REFERENCES `CoachConnect`.`Usuario` (`idUsuario`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-CREATE INDEX `fk_Estudiante_Usuario1_idx` ON `BookingTutorias`.`Estudiante` (`Usuario_idUsuario` ASC) VISIBLE;
+CREATE INDEX `fk_Estudiante_Usuario1_idx` ON `CoachConnect`.`Estudiante` (`Usuario_idUsuario` ASC) VISIBLE;
 
 
 -- -----------------------------------------------------
--- Table `BookingTutorias`.`Tutor`
+-- Table `CoachConnect`.`Tutor`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `BookingTutorias`.`Tutor` (
+CREATE TABLE IF NOT EXISTS `CoachConnect`.`Tutor` (
   `idTutor` INT NOT NULL AUTO_INCREMENT,
   `Profesion` VARCHAR(45) NOT NULL,
   `Descripcion` VARCHAR(200) NOT NULL,
@@ -62,63 +61,63 @@ CREATE TABLE IF NOT EXISTS `BookingTutorias`.`Tutor` (
   PRIMARY KEY (`idTutor`, `Usuario_idUsuario`),
   CONSTRAINT `fk_Tutor_Usuario`
     FOREIGN KEY (`Usuario_idUsuario`)
-    REFERENCES `BookingTutorias`.`Usuario` (`idUsuario`)
+    REFERENCES `CoachConnect`.`Usuario` (`idUsuario`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-CREATE INDEX `fk_Tutor_Usuario_idx` ON `BookingTutorias`.`Tutor` (`Usuario_idUsuario` ASC) VISIBLE;
+CREATE INDEX `fk_Tutor_Usuario_idx` ON `CoachConnect`.`Tutor` (`Usuario_idUsuario` ASC) VISIBLE;
 
 
 -- -----------------------------------------------------
--- Table `BookingTutorias`.`Admin`
+-- Table `CoachConnect`.`Admin`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `BookingTutorias`.`Admin` (
+CREATE TABLE IF NOT EXISTS `CoachConnect`.`Admin` (
   `idAdmin` INT NOT NULL AUTO_INCREMENT,
   `Usuario_idUsuario` INT NOT NULL,
   PRIMARY KEY (`idAdmin`, `Usuario_idUsuario`),
   CONSTRAINT `fk_Admin_Usuario1`
     FOREIGN KEY (`Usuario_idUsuario`)
-    REFERENCES `BookingTutorias`.`Usuario` (`idUsuario`)
+    REFERENCES `CoachConnect`.`Usuario` (`idUsuario`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-CREATE INDEX `fk_Admin_Usuario1_idx` ON `BookingTutorias`.`Admin` (`Usuario_idUsuario` ASC) VISIBLE;
+CREATE INDEX `fk_Admin_Usuario1_idx` ON `CoachConnect`.`Admin` (`Usuario_idUsuario` ASC) VISIBLE;
 
 
 -- -----------------------------------------------------
--- Table `BookingTutorias`.`Detalle_Reserva`
+-- Table `CoachConnect`.`Detalle_Reserva`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `BookingTutorias`.`Detalle_Reserva` (
+CREATE TABLE IF NOT EXISTS `CoachConnect`.`Detalle_Reserva` (
   `idDetalle_Reserva` INT NOT NULL AUTO_INCREMENT,
-  `Fecha_Inicio_Reserva` DATETIME NOT NULL,
-  `Fecha_Fin_Reserva` DATETIME NOT NULL,
-  `Horas_Reservadas` FLOAT NOT NULL,
+  `FechaInicioReserva` DATETIME NOT NULL,
+  `FechaFinReserva` DATETIME NOT NULL,
+  `HorasReservadas` INT NOT NULL,
   `Estudiante_idEstudiante` INT NOT NULL,
   `Tutor_idTutor` INT NOT NULL,
   PRIMARY KEY (`idDetalle_Reserva`, `Estudiante_idEstudiante`, `Tutor_idTutor`),
   CONSTRAINT `fk_Detalle_Reserva_Estudiante1`
     FOREIGN KEY (`Estudiante_idEstudiante`)
-    REFERENCES `BookingTutorias`.`Estudiante` (`idEstudiante`)
+    REFERENCES `CoachConnect`.`Estudiante` (`idEstudiante`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Detalle_Reserva_Tutor1`
     FOREIGN KEY (`Tutor_idTutor`)
-    REFERENCES `BookingTutorias`.`Tutor` (`idTutor`)
+    REFERENCES `CoachConnect`.`Tutor` (`idTutor`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-CREATE INDEX `fk_Detalle_Reserva_Estudiante1_idx` ON `BookingTutorias`.`Detalle_Reserva` (`Estudiante_idEstudiante` ASC) VISIBLE;
+CREATE INDEX `fk_Detalle_Reserva_Estudiante1_idx` ON `CoachConnect`.`Detalle_Reserva` (`Estudiante_idEstudiante` ASC) VISIBLE;
 
-CREATE INDEX `fk_Detalle_Reserva_Tutor1_idx` ON `BookingTutorias`.`Detalle_Reserva` (`Tutor_idTutor` ASC) VISIBLE;
+CREATE INDEX `fk_Detalle_Reserva_Tutor1_idx` ON `CoachConnect`.`Detalle_Reserva` (`Tutor_idTutor` ASC) VISIBLE;
 
 
 -- -----------------------------------------------------
--- Table `BookingTutorias`.`Nivel`
+-- Table `CoachConnect`.`Nivel`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `BookingTutorias`.`Nivel` (
+CREATE TABLE IF NOT EXISTS `CoachConnect`.`Nivel` (
   `idNivel` INT NOT NULL AUTO_INCREMENT,
   `Nombre` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`idNivel`))
@@ -126,9 +125,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `BookingTutorias`.`Tutoria`
+-- Table `CoachConnect`.`Tutoria`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `BookingTutorias`.`Tutoria` (
+CREATE TABLE IF NOT EXISTS `CoachConnect`.`Tutoria` (
   `idTutoria` INT NOT NULL AUTO_INCREMENT,
   `Nombre` VARCHAR(45) NOT NULL,
   `Descripcion` VARCHAR(200) NOT NULL,
@@ -136,36 +135,36 @@ CREATE TABLE IF NOT EXISTS `BookingTutorias`.`Tutoria` (
   PRIMARY KEY (`idTutoria`, `Nivel_idNivel`),
   CONSTRAINT `fk_Tutoria_Nivel1`
     FOREIGN KEY (`Nivel_idNivel`)
-    REFERENCES `BookingTutorias`.`Nivel` (`idNivel`)
+    REFERENCES `CoachConnect`.`Nivel` (`idNivel`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-CREATE INDEX `fk_Tutoria_Nivel1_idx` ON `BookingTutorias`.`Tutoria` (`Nivel_idNivel` ASC) VISIBLE;
+CREATE INDEX `fk_Tutoria_Nivel1_idx` ON `CoachConnect`.`Tutoria` (`Nivel_idNivel` ASC) VISIBLE;
 
 
 -- -----------------------------------------------------
--- Table `BookingTutorias`.`Tutor_Tutoria`
+-- Table `CoachConnect`.`Tutor_Tutoria`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `BookingTutorias`.`Tutor_Tutoria` (
+CREATE TABLE IF NOT EXISTS `CoachConnect`.`Tutor_Tutoria` (
   `Tutor_idTutor` INT NOT NULL,
   `Tutoria_idTutoria` INT NOT NULL,
   PRIMARY KEY (`Tutor_idTutor`, `Tutoria_idTutoria`),
   CONSTRAINT `fk_Tutor_has_Tutoria_Tutor1`
     FOREIGN KEY (`Tutor_idTutor`)
-    REFERENCES `BookingTutorias`.`Tutor` (`idTutor`)
+    REFERENCES `CoachConnect`.`Tutor` (`idTutor`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Tutor_has_Tutoria_Tutoria1`
     FOREIGN KEY (`Tutoria_idTutoria`)
-    REFERENCES `BookingTutorias`.`Tutoria` (`idTutoria`)
+    REFERENCES `CoachConnect`.`Tutoria` (`idTutoria`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-CREATE INDEX `fk_Tutor_has_Tutoria_Tutoria1_idx` ON `BookingTutorias`.`Tutor_Tutoria` (`Tutoria_idTutoria` ASC) VISIBLE;
+CREATE INDEX `fk_Tutor_has_Tutoria_Tutoria1_idx` ON `CoachConnect`.`Tutor_Tutoria` (`Tutoria_idTutoria` ASC) VISIBLE;
 
-CREATE INDEX `fk_Tutor_has_Tutoria_Tutor1_idx` ON `BookingTutorias`.`Tutor_Tutoria` (`Tutor_idTutor` ASC) VISIBLE;
+CREATE INDEX `fk_Tutor_has_Tutoria_Tutor1_idx` ON `CoachConnect`.`Tutor_Tutoria` (`Tutor_idTutor` ASC) VISIBLE;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
