b46742345f974630d196cf3d518f5d179005acc5
darwi <darwinsosa2004@gmail.com>
2024-02-23 00:01:24 -0300

    19: added tutor controller and dtos

---
+++ src/main/java/com/digitalhouse/CoachConnectBE/controller/tutor/dto/NuevoTutorDto.java
@@ -0,0 +1,19 @@
+package com.digitalhouse.CoachConnectBE.controller.tutor.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class NuevoTutorDto {
+    private String nombre;
+    private String apellido;
+    private int edad;
+    private String email;
+    private String contactoCelular;
+    private String foto;
+    private String username;
+    private String profesion;
+
+    private String descripcion;
+}
---
+++ src/main/java/com/digitalhouse/CoachConnectBE/controller/tutor/dto/TutorResultadoDto.java
@@ -0,0 +1,20 @@
+package com.digitalhouse.CoachConnectBE.controller.tutor.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class TutorResultadoDto {
+    private Long id;
+    private String nombre;
+    private String apellido;
+    private int edad;
+    private String email;
+    private String contactoCelular;
+    private String foto;
+    private String username;
+    private String profesion;
+
+    private String descripcion;
+}
