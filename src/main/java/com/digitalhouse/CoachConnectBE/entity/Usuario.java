2d3dbe16d5e54e41463f33cd8aff5f4ea5bec35f
darwi <darwinsosa2004@gmail.com>
2024-02-22 23:16:26 -0300

    18: added Entity for usuario and estudiante

---
+++ src/main/java/com/digitalhouse/CoachConnectBE/entity/Usuario.java
@@ -0,0 +1,34 @@
+package com.digitalhouse.CoachConnectBE.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "USUARIO")
+@Getter
+@Setter
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+
+    private String apellido;
+
+    private int edad;
+
+    private String email;
+
+    @Column(name = "contacto_celular")
+    private String contactoCelular;
+
+    private String foto;
+
+    private String username;
+
+    private String password;
+}
