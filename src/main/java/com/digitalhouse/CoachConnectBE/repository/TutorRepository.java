f19eb23532f3e0ad8e1de94890279187bd7a9e13
darwi <darwinsosa2004@gmail.com>
2024-02-23 00:00:29 -0300

    19: added tutor repository

---
+++ src/main/java/com/digitalhouse/CoachConnectBE/repository/TutorRepository.java
@@ -0,0 +1,31 @@
+package com.digitalhouse.CoachConnectBE.repository;
+
+
+import com.digitalhouse.CoachConnectBE.entity.Tutor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Repository
+public interface TutorRepository extends JpaRepository<Tutor,Long> {
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE Tutor t " +
+            "SET t.profesion = :profesion, " +
+            "t.descripcion = :descripcion " +
+            "WHERE t.id = :id")
+    void update(
+            @Param("id") Long id,
+            @Param("profesion") String profesion,
+            @Param("descripcion") String descripcion
+    );
+
+
+
+    Optional<Tutor> findTutorById(Long id);
+}
