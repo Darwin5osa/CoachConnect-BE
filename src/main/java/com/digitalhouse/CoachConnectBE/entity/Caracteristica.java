7284a818be59be3a3d4b9adda695a96ae0f3a147
Darwin Sosa <103063905+Darwin5osa@users.noreply.github.com>
2024-03-07 16:51:35 -0300

    added import

--- src/main/java/com/digitalhouse/CoachConnectBE/entity/Caracteristica.java
+++ src/main/java/com/digitalhouse/CoachConnectBE/entity/Caracteristica.java
@@ -1,5 +1,6 @@
 package com.digitalhouse.CoachConnectBE.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
