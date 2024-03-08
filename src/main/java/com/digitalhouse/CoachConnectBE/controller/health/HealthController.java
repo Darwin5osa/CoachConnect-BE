77cfe5e1b229eac6425d91c62f2338192061eb90
Maureen <110852730+MCristina94@users.noreply.github.com>
2024-03-07 23:20:16 -0500

    se importa crossorigin

--- src/main/java/com/digitalhouse/CoachConnectBE/controller/health/HealthController.java
+++ src/main/java/com/digitalhouse/CoachConnectBE/controller/health/HealthController.java
@@ -2,6 +2,7 @@ package com.digitalhouse.CoachConnectBE.controller.health;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
