8a13f70c65d776f01f38b9e19ab12e9360753ba5
Maureen <110852730+MCristina94@users.noreply.github.com>
2024-03-07 23:14:19 -0500

    agregar el cors para www

--- src/main/java/com/digitalhouse/CoachConnectBE/controller/health/HealthController.java
+++ src/main/java/com/digitalhouse/CoachConnectBE/controller/health/HealthController.java
@@ -7,11 +7,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "https://www.coachconnect.tech")
 @RequestMapping("/status")
 @Slf4j
 public class HealthController {
     @GetMapping()
     public ResponseEntity<String> status() {
-        return ResponseEntity.ok("Hola Mundo");
+        return ResponseEntity.ok("Hello World");
     }
 }
