0d7368ee645e5e168b72ab6fcbc1f05638705ab0
darwi <darwinsosa2004@gmail.com>
2024-03-31 19:12:26 -0300

    changed the delete way, to use estudianteId and tutoriaId

--- src/main/java/com/digitalhouse/CoachConnectBE/repository/FavoritoRepository.java
+++ src/main/java/com/digitalhouse/CoachConnectBE/repository/FavoritoRepository.java
@@ -2,9 +2,11 @@ package com.digitalhouse.CoachConnectBE.repository;
 
 import com.digitalhouse.CoachConnectBE.entity.Favorito;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -12,4 +14,9 @@ import java.util.List;
 public interface FavoritoRepository extends JpaRepository<Favorito,Long> {
     @Query("SELECT f FROM Favorito f WHERE f.estudiante.id = :estudianteId")
     List<Favorito> findByEstudianteId(@Param("estudianteId") Long estudianteId);
+
+    @Transactional
+    @Modifying
+    @Query("DELETE from Favorito f WHERE f.estudiante.id = :estudianteId AND f.tutoria.id = :tutoriaId")
+    void deleteByEstudianteIdAndTutoriaId(@Param("estudianteId") Long estudianteId, @Param("tutoriaId") Long tutoriaId);
 }
